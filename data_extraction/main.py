c5fa9998ee59e485c10c9f57badae1d7736c4487
T-Terra <gabst138@gmail.com>
2025-03-25 20:09:04 -0300

    feat: code resolution ok

--- data_extraction/main.py
+++ data_extraction/main.py
@@ -1,35 +1,45 @@
-import pymupdf  # PyMuPDF
-import csv
+import tabula
+import PyPDF2
 from os import getcwd
-import re
 
 # Função para extrair texto do PDF e salvar em CSV
-def extract_table_from_pdf(pdf_path, output_csv_path):
+def main(pdf_path, output_csv_path):
     # Abrir o documento PDF
-    doc = pymupdf.open(pdf_path)
-    
-    # Criar ou abrir o arquivo CSV para escrita
-    with open(output_csv_path, 'w', newline='', encoding='utf-8') as csv_file:
-        writer = csv.writer(csv_file)
-        
-        # Iterar pelas páginas do documento
-        for page_num in range(len(doc)):
-            page = doc.load_page(page_num)  # Carregar a página
-            text = page.get_text("text")  # Obter o texto da página
-            
-            # Regex para identificar linhas da tabela
-            # Exemplo: captura de linhas com 3 colunas separadas por espaços ou tabulações
-            # Ajuste a regex conforme a estrutura real do seu PDF
-            pattern = r"(\S+)\s+(\S+)\s+(\S+)"  # Regex para capturar 3 colunas (modifique conforme necessário)
-            matches = re.findall(pattern, text)
-            
-            # Escrever os dados extraídos no CSV
-            for match in matches:
-                writer.writerow(match)  # Escrever cada linha no arquivo CSV
 
-# Caminho para o arquivo PDF e o arquivo CSV de saída
+    with open(pdf_path, "rb") as file:
+        reader = PyPDF2.PdfReader(file)
+        total_pages = len(reader.pages)
+        
+    pages = 3
+
+    while True:
+        if pages > total_pages:
+            break
+
+        doc = tabula.read_pdf(pdf_path, pages=pages, lattice=True)
+        if len(doc) == 1:
+            if pages == 3:
+                print(pages)
+                table = doc[0]
+                table.to_csv(output_csv_path, mode="a", index=False)
+                pages += 1
+            else:
+                print(pages)
+                table = doc[0]
+                table.to_csv(output_csv_path, mode="a", index=False, header=False)
+                pages += 1
+        elif len(doc) > 1:
+            print(len(doc), f"page: {pages}")
+            table = doc[0]
+            table.to_csv("maior.csv", mode="a", index=False)
+            pages += 1
+        else:
+            break
+
+
 pdf_path = f"{getcwd()}\\downloads\\files\\Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf"
 csv_output_path = "output_table.csv"
 
-# Chamar a função para extrair a tabela e salvar em CSV
-extract_table_from_pdf(pdf_path, csv_output_path)
+if __name__ == '__main__':
+    # Chamar a função para extrair a tabela e salvar em CSV
+    main(pdf_path, csv_output_path)
