602837f263236527c7f6e223a668291652da72af
T-Terra <gabst138@gmail.com>
2025-03-28 00:44:13 -0300

    fix: config settings backend allowed hosts

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-_@*ng+h5c^+54$3tv_snizps6y&axj==53*^#x1tt8=q!lj-ap
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = ["estagio-teste-five.vercel.app", "localhost:5173"]
+ALLOWED_HOSTS = ["estagio-teste-five.vercel.app", "localhost"]
 
 APPEND_SLASH = False
 
