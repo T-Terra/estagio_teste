143782a14f17c101031133523aca4bae16ae2ba7
T-Terra <gabst138@gmail.com>
2025-03-29 13:01:35 -0300

    chore: response backend return 404

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,6 +3,7 @@ from .serializer import EnterpriseSerializer
 from .filters import EnterpriseFilters
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_404_NOT_FOUND
 
 
 class ApiViewset(ViewSet):
@@ -10,4 +11,7 @@ class ApiViewset(ViewSet):
         queryset = Enterprise.objects.all()
         query_filter = EnterpriseFilters(request.GET, queryset)
         serializer = EnterpriseSerializer(query_filter.qs[:10], many=True)
-        return Response(serializer.data)
+        if len(serializer.data) == 0:
+            return Response(serializer.data, status=HTTP_404_NOT_FOUND)
+        else:
+            return Response(serializer.data, status=HTTP_200_OK)
