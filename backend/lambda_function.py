1cfe22a12e32c7b1940d29c191260eb35ccdfff1
T-Terra <gabst138@gmail.com>
2025-03-29 00:35:33 -0300

    chore: add logging info in lambda func

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -1,16 +1,37 @@
 import os
 import django
+import logging
 from django.core.wsgi import get_wsgi_application
-import awsgi 
-# Define as variáveis de ambiente do Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+import awsgi
 
-# Inicializa o Django
-django.setup()
+# Configuração do logging para depuração
+logging.basicConfig(level=logging.INFO)
 
-# Gera o aplicativo WSGI do Django
-application = get_wsgi_application()
+try:
+    # Definir as variáveis de ambiente do Django
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+    # Inicializa o Django
+    django.setup()
+
+    # Gera o aplicativo WSGI do Django
+    application = get_wsgi_application()
+
+    # Log de sucesso da inicialização
+    logging.info("Django inicializado com sucesso.")
+
+except Exception as e:
+    logging.error("Erro ao inicializar o Django: %s", str(e))
+    raise e
 
 # Este será o ponto de entrada da função Lambda
 def lambda_handler(event, context):
-    return awsgi.response(application, event, context)
+    try:
+        # Retorna a resposta usando o awsgi
+        response = awsgi.response(application, event, context)
+        logging.info("Resposta gerada com sucesso.")
+        return response
+    except Exception as e:
+        logging.error("Erro durante a execução do Lambda: %s", str(e))
+        raise e
+
